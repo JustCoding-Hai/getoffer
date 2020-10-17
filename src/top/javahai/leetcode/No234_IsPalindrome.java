eb984dcd118fd7c062bc5b02d4a3f763f0444caa
hai_global <jinhaihuang824@aliyun.com>
2020-10-17 18:27:02 +0800

    234. 回文链表

--- src/top/javahai/leetcode/No234_IsPalindrome.java
+++ src/top/javahai/leetcode/No234_IsPalindrome.java
@@ -1,6 +1,5 @@
 package top.javahai.leetcode;
 
-import java.util.List;
 
 /**
  * @author Hai
@@ -13,13 +12,14 @@ public class No234_IsPalindrome {
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
-        ListNode l1 = new ListNode(1);
+        ListNode l1 = new ListNode(2);
         head.next=l1;
         ListNode l2 = new ListNode(2);
         l1.next=l2;
         ListNode l3 = new ListNode(1);
         l2.next=l3;
         System.out.println(isPalindrome(head));
+        System.out.println(isPalindrome2(head));
 
     }
     /**
@@ -78,5 +78,50 @@ public class No234_IsPalindrome {
             raw=raw.next;
         }
         return true;
+    }
+
+    /**
+     * 上面方法的改进，反转一半链表然后与原来的链表进行比较
+     * @param head
+     * @return
+     */
+    public static boolean isPalindrome2(ListNode head) {
+       if (head==null||head.next==null){
+            return true;
+        }
+        int length=0;
+        //遍历链表的指针
+        ListNode current=head;
+        while (current!=null){
+            length++;
+            current=current.next;
+        }
+        int half=length/2;
+        int count=0;
+        //反转前半部分的链表
+        ListNode reverse=new ListNode();
+        current=head;
+        while (count<half&&current.next!=null){
+            ListNode next=current.next;
+            current.next=reverse.next;
+            reverse.next=current;
+            current=next;
+            count++;
+        }
+        //如果是奇数，就不要中间的数，后半部分链表不需要第一个
+        if (length%2!=0){
+            current= current.next;
+        }
+        //前半部分链表和后半部分比较是否相同
+        reverse=reverse.next;
+        while (reverse!=null&current!=null){
+            if (reverse.val!=current.val){
+                return false;
+            }
+            reverse=reverse.next;
+            current=current.next;
+        }
+        return true;
+
     }
 }
