4e8b5943c46fc1fb213304021fc444d90249e6c5
hai_global <jinhaihuang824@aliyun.com>
2020-11-18 00:06:32 +0800

    剑指 Offer 32 - I. 从上到下打印二叉树-递归实现

--- src/top/javahai/togetoffer/No32_LevelOrder.java
+++ src/top/javahai/togetoffer/No32_LevelOrder.java
@@ -1,9 +1,8 @@
 package top.javahai.togetoffer;
 
-import java.awt.*;
-import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Hai
@@ -31,8 +30,9 @@ public class No32_LevelOrder {
      * [3,9,20,15,7]
      *
      * 实现：
-     * 使用Queue来完成
+     * 借助Queue来完成
      *
+     * 层序遍历（广度优先搜索BFS）：
      *1.从左到右遍历将每一层结点添加到队列中，获取结点的同时删除
      * 2.当队列为空的时候，则说明遍历完了二叉树
      *
@@ -54,10 +54,10 @@ public class No32_LevelOrder {
                 TreeNode node = linkedList.poll();
                 nums.add(node.val);
                 if (node.left!=null){
-                    linkedList.add(node.left);
+                    linkedList.offer(node.left);
                 }
                 if (node.right!=null){
-                    linkedList.add(node.right);
+                    linkedList.offer(node.right);
                 }
                 size--;
             }
@@ -69,4 +69,46 @@ public class No32_LevelOrder {
         }
         return result;
     }
+
+
+
+    /**
+     *
+     * 递归实现：
+     * 1.使用List<List<Integer>>，其中List<Integer>用于存储每一层的结点的数值。
+     * 2.递归的时候，记录层数，每下一层height+1，根据height来保存数值到List<Integer>中
+     * 3.这样，先递归左子树，再递归右子树
+     * @param root
+     * @return
+     */
+    public int[] levelOrder2(TreeNode root) {
+        //每一层的从左到右的结点数值
+        List<List<Integer>> list = new ArrayList<>();
+        levelHelper(list, root, 0);
+        //将每个结点的数值按顺序放入到数值集合中
+        List<Integer> tempList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            tempList.addAll(list.get(i));
+        }
+
+        //把list转化为数组
+        int[] res = new int[tempList.size()];
+        for (int i = 0; i < tempList.size(); i++) {
+            res[i] = tempList.get(i);
+        }
+        return res;
+    }
+
+    public void levelHelper(List<List<Integer>> list, TreeNode root, int height) {
+        if (root == null){
+            return;
+        }
+        if (height >= list.size()) {
+            list.add(new ArrayList<>());
+        }
+        levelHelper(list, root.left, height + 1);
+        list.get(height).add(root.val);
+        levelHelper(list, root.right, height + 1);
+    }
+
 }
