6378628aaedbe8da669be928fbe87d230a749fad
hai_global <jinhaihuang824@aliyun.com>
2021-02-22 23:42:22 +0800

    766. 托普利茨矩阵

---
+++ src/top/javahai/leetcode/No766_IsToeplitzMatrix.java
@@ -0,0 +1,26 @@
+package top.javahai.leetcode;
+
+/**
+ * @author Hai
+ * @program: getoffer
+ * @description: 766. 托普利茨矩阵
+ * @create 2021/2/22 - 23:28
+ **/
+public class No766_IsToeplitzMatrix {
+
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        if (matrix.length < 2) {
+            return false;
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+}
